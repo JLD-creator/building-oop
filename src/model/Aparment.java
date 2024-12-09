5cc8be24375c55707423bc90b79db037830627b7
Jesus <jesus@b25>
2024-12-09 09:08:23 +0100

    Pojos echos

--- src/model/Aparment.java
+++ src/model/Aparment.java
@@ -1,4 +1,62 @@
 package model;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Aparment {
+    private int plant;
+    private String door;
+    private Propietary[] propietary;
+
+    public Aparment(int plant, String door, Propietary[] propietary) {
+        this.plant = plant;
+        this.door = door;
+        this.propietary = propietary;
+    }
+
+    public int getPlant() {
+        return plant;
+    }
+
+    public void setPlant(int plant) {
+        this.plant = plant;
+    }
+
+    public String getDoor() {
+        return door;
+    }
+
+    public void setDoor(String door) {
+        this.door = door;
+    }
+
+    public Propietary[] getPropietary() {
+        return propietary;
+    }
+
+    public void setPropietary(Propietary[] propietary) {
+        this.propietary = propietary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aparment aparment = (Aparment) o;
+        return plant == aparment.plant && Objects.equals(door, aparment.door) && Objects.deepEquals(propietary, aparment.propietary);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(plant, door, Arrays.hashCode(propietary));
+    }
+
+    @Override
+    public String toString() {
+        return "Aparment{" +
+                "plant=" + plant +
+                ", door='" + door + '\'' +
+                ", propietary=" + Arrays.toString(propietary) +
+                '}';
+    }
 }
--- src/model/Building.java
+++ src/model/Building.java
@@ -1,4 +1,62 @@
 package model;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Building {
+    private String direction;
+    private String municipiality;
+    private Aparment[] aparment;
+
+    public Building(String direction, String municipiality, Aparment[] aparment) {
+        this.direction = direction;
+        this.municipiality = municipiality;
+        this.aparment = aparment;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public String getMunicipiality() {
+        return municipiality;
+    }
+
+    public void setMunicipiality(String municipiality) {
+        this.municipiality = municipiality;
+    }
+
+    public Aparment[] getAparment() {
+        return aparment;
+    }
+
+    public void setAparment(Aparment[] aparment) {
+        this.aparment = aparment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Building building = (Building) o;
+        return Objects.equals(direction, building.direction) && Objects.equals(municipiality, building.municipiality) && Objects.deepEquals(aparment, building.aparment);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(direction, municipiality, Arrays.hashCode(aparment));
+    }
+
+    @Override
+    public String toString() {
+        return "Building{" +
+                "direction='" + direction + '\'' +
+                ", municipiality='" + municipiality + '\'' +
+                ", aparment=" + Arrays.toString(aparment) +
+                '}';
+    }
 }
