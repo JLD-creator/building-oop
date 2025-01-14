309571228e3a7c57ce8fab071284cc7c56b2cd0a
Jesus <jesus@b25>
2025-01-14 10:56:42 +0100

    Menu completado

--- src/components/BuildingAppMenu.java
+++ src/components/BuildingAppMenu.java
@@ -17,38 +17,57 @@ public class BuildingAppMenu {
         Building building = buildingReader.read();
         int option;
         do {
-            System.out.println("1. Mostrar información del edificio");
-            System.out.println("2. Mostrar apartamentos de una planta");
-            System.out.println("3. Mostrar datos segun planta y puerta");
-            System.out.println("4. Mostrar propietarios de apartamentos segun puerta y planta");
-            System.out.println("5. Salir");
-            System.out.println("Elige una opción");
-            option = scanner.nextInt();
+            option = chooseOption();
             if (option == 1) {
                 building.showInfo();
             } else if (option == 2) {
-                System.out.println("Introduce una planta");
-                int floor = scanner.nextInt();
-                scanner.nextLine();
-                building.showFloorAparments(floor);
+                option2(building);
 
             } else if (option == 3) {
-                System.out.println("Introduce una planta");
-                int plant = scanner.nextInt();
-                scanner.nextLine();
-                System.out.println("Introduce una puerta");
-                String door = scanner.nextLine();
-                building.findOwners(plant, door);
+                option3(building);
             } else if (option == 4) {
-                System.out.println("Introduce una planta");
-                int plant = scanner.nextInt();
-                scanner.nextLine();
-                System.out.println("Introduce una puerta");
-                String door = scanner.nextLine();
-                building.findAparment(door, plant);
+                option4(building);
             } else if (option == 5) {
                 System.out.println("Saliendo ...");
             }
         } while (option != 5);
+    }
+
+    private void option4(Building building) {
+        System.out.println("Introduce una planta");
+        int plant = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Introduce una puerta");
+        String door = scanner.nextLine();
+        building.findAparment(door, plant);
+    }
+
+    private void option3(Building building) {
+        System.out.println("Introduce una planta");
+        int plant = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Introduce una puerta");
+        String door = scanner.nextLine();
+        building.findOwners(plant, door);
+    }
+
+    private void option2(Building building) {
+        System.out.println("Introduce una planta");
+        int floor = scanner.nextInt();
+        scanner.nextLine();
+        building.showFloorAparments(floor);
+    }
+
+    private int chooseOption() {
+        int option;
+        System.out.println("1. Mostrar información del edificio");
+        System.out.println("2. Mostrar apartamentos de una planta");
+        System.out.println("3. Mostrar datos segun planta y puerta");
+        System.out.println("4. Mostrar propietarios de apartamentos segun puerta y planta");
+        System.out.println("5. Salir");
+        System.out.println("Elige una opción");
+        option = scanner.nextInt();
+        scanner.nextLine();
+        return option;
     }
 }
